6aa134f87a9a76b7d00afac797a7bbae05865f17
vincentm1214 <144461068+vincentm1214@users.noreply.github.com>
2024-03-08 10:07:19 -0500

    fyuo

--- vite-project/src/router/index.js
+++ vite-project/src/router/index.js
@@ -10,12 +10,14 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/barchart',
+      name: 'barchart',
+      component: () => import('../views/BarChart.vue')
+    },
+    {
+      path: '/doughnutgraph',
+      name: 'doughnutgraph',
+      component: () => import('../views/DoughnutGraph.vue')
     }
   ]
 })
--- vite-project/src/views/HomeView.vue
+++ vite-project/src/views/HomeView.vue
@@ -1,9 +1,24 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+import{ref, onMounted} from "vue";
+  const skull= ref("");
+  async function getskull(){
+    let res= await fetch("https://data.cityofnewyork.us/resource/jb7j-dtam.json");
+    let data= await res.json();
+    skull.value = data;
+  }
+
+onMounted(()=> {
+  getskull();
+});
+
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style scoped>
+
+</style>
