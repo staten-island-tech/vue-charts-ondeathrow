d19ce82d5523f2b1dbb35b5aed983f5434d757cb
samanthay27 <144460844+samanthay27@users.noreply.github.com>
2024-03-22 09:31:07 -0400

    hehe

--- vite-project/src/views/DoughnutGraph.vue
+++ vite-project/src/views/DoughnutGraph.vue
@@ -1,16 +1,30 @@
 <template>
-  <div class="container">
-  </div>
+  <Bar
+    id="my-chart-id"
+    :options="chartOptions"
+    :data="chartData"
+  />
 </template>
 
-<script setup>
-import { Doughnut } from 'vue-chartjs'
+<script>
+import { Bar } from 'vue-chartjs'
 import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+
 ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
 
-
+export default {
+  name: 'BarChart',
+  components: { Bar },
+  data() {
+    return {
+      chartData: {
+        labels: [ '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014' ],
+        datasets: [ { data: [6487, 2540, 4512, 7363, 5837, 7887,8978,3354] } ]
+      },
+      chartOptions: {
+        responsive: true
+      }
+    }
+  }
+}
 </script>
-
-<style scoped>
-
-</style>
